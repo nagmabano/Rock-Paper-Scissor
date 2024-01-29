80e6dc91655669d7d297cc96dd1fa7497b4eb4cf
Nagma Bano <nagma.bano@sap.com>
2024-01-29 19:36:45 +0530

    rock paper scissor game

--- app/src/main/java/com/learning/rockpaperscissor/RockPaperScissor.kt
+++ app/src/main/java/com/learning/rockpaperscissor/RockPaperScissor.kt
@@ -1,38 +1,45 @@
 package com.learning.rockpaperscissor
 
 fun main() {
+
     var computerChoice = ""
     var playerChoice = ""
 
     println("Rock, Paper or Scissor? Enter your choice!")
     playerChoice = readln()
 
-    val randomNumber = (1..3).random()
-    computerChoice = when (randomNumber) {
-        1 -> {
-            "Rock"
+    while(playerChoice == "Rock" || playerChoice == "Paper" || playerChoice == "Scissor") {
+        val randomNumber = (1..3).random()
+        computerChoice = when (randomNumber) {
+            1 -> {
+                "Rock"
+            }
+            2 -> {
+                "Paper"
+            }
+            else -> {
+                "Scissor"
+            }
         }
-        2 -> {
-            "Paper"
-        }
-        else -> {
-            "Scissor"
-        }
-    }
-    println(computerChoice)
+        println(computerChoice)
 
-    val winner = when {
-        playerChoice == computerChoice -> "Tie"
-        playerChoice == "Rock" && computerChoice == "Scissor" -> "Player"
-        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
-        playerChoice == "Scissor" && computerChoice == "Paper" -> "Player"
-        else -> "Computer"
+        val winner = when {
+            playerChoice == computerChoice -> "Tie"
+            playerChoice == "Rock" && computerChoice == "Scissor" -> "Player"
+            playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+            playerChoice == "Scissor" && computerChoice == "Paper" -> "Player"
+            else -> "Computer"
+        }
+
+        if(winner == "Tie") {
+            println("It's a tie")
+        } else {
+            println("The winner is $winner")
+        }
+
+        playerChoice = readln()
     }
 
-    if(winner == "Tie") {
-        println("It's a tie")
-    } else {
-        println("The winner is $winner")
-    }
+    println("Game End!!")
 
 }
