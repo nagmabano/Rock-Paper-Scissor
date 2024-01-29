b71876c5681607b1e139ca286fd0c8c14e647996
Nagma Bano <nagma.bano@sap.com>
2024-01-29 19:22:18 +0530

    running code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.learning.rockpaperscissor"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.learning.rockpaperscissor"
